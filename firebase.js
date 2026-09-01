8ab05d17a2a8c5bbcc0a47ae858d1cbda8d757e2
s81987269-star <s81987269@gmail.com>
2026-09-01 22:37:11 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBdA3mGe6NJ1SBw0j7xeEXPC4mMgsdQs3k",
   authDomain: "esports-tournaments-c6628.firebaseapp.com",
-  databaseURL: "https://esports-tournaments-c6628-default-rtdb.firebaseio.com",
   projectId: "esports-tournaments-c6628",
-  storageBucket: "esports-tournaments-c6628.firebasestorage.app",
+  databaseURL: "https://esports-tournaments-c6628-default-rtdb.firebaseio.com",
   messagingSenderId: "520071842202",
   appId: "1:520071842202:web:6867f5c63fa032e7135223"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+const rtdb = firebase.database();
+const storage = firebase.storage();
